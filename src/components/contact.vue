8aeaa80783073d3f6ad575f98a384d5d868128ab
blackomilk <siyonghang@365rili.com>
2021-03-19 17:19:02 +0800

    mm

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -17,8 +17,7 @@ export default {
   height: 100vh;
   background-color: #ffcd00;
   padding-top: 1.1rem;
-  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
-  font-weight: 700;
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
   font-size: 1.2rem;
   display: flex;
   flex-direction: column;
@@ -34,57 +33,7 @@ export default {
   z-index: 1;
   animation: 5s rotate ease-in-out infinite;
 }
-.foo {
-  padding: 0;
-  margin: 0;
-  position: relative;
-  color: #fff;
-  letter-spacing: 0.01rem;
-  font-size: 1rem;
-  z-index: 1;
-  animation: 2.5s foo-ratote ease-in-out infinite;
-}
-@keyframes foo-ratote {
-  0%,
-  100% {
-    transform: rotate3d(1, 0, 0, -20deg);
-  }
-  50% {
-    transform: rotate3d(1, 0, 0, 20deg);
-  }
-}
-.foo::before,
-.foo::after {
-  content: "Thank You!";
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -1;
-}
-.foo::before {
-  color: #009cde;
-  animation: 2.5s foo-before ease-in-out infinite;
-}
-.foo::after {
-  color: #003c71;
-  animation: 2.5s foo-after ease-in-out infinite;
-}
-@keyframes foo-before {
-  0%,100% {
-    transform: translate3d(1px, 3px, -2.5px);
-  }
-  50% {
-    transform: translate3d(1px, -3px, -2.5px);
-  }
-}
-@keyframes foo-after {
-  0%,100% {
-    transform: translate3d(1px, 5px, -2.5px);
-  }
-  50% {
-    transform: translate3d(1px, -5px, -2.5px);
-  }
-}
+
 .title::before,
 .title::after {
   content: "Contact Me";
@@ -114,10 +63,10 @@ export default {
 @keyframes title-before {
   0%,
   100% {
-    transform: translate3d(2.5px, 2px, -2.5px);
+    transform: translate3d(3px, 2px, -2.5px);
   }
   50% {
-    transform: translate3d(-2.5px, 2px, -2.5px);
+    transform: translate3d(-3px, 2px, -2.5px);
   }
 }
 @keyframes title-after {
@@ -127,6 +76,60 @@ export default {
   }
   50% {
     transform: translate3d(-5px, 4px, -5px);
+  }
+}
+.foo {
+  padding: 0;
+  margin: 0;
+  position: relative;
+  color: #fff;
+  letter-spacing: 0.01rem;
+  font-size: 1rem;
+  z-index: 1;
+  animation: 2.5s foo-ratote ease-in-out infinite;
+}
+@keyframes foo-ratote {
+  0%,
+  100% {
+    transform: rotate3d(1, 0, 0, -10deg);
+  }
+  50% {
+    transform: rotate3d(1, 0, 0, 15deg);
+  }
+}
+.foo::before,
+.foo::after {
+  content: "Thank You!";
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.foo::before {
+  color: #009cde;
+  animation: 2.5s foo-before ease-in-out infinite;
+    z-index: -1;
+
+}
+.foo::after {
+  color: #003c71;
+  animation: 2.5s foo-after ease-in-out infinite;
+    z-index: -2;
+
+}
+@keyframes foo-before {
+  0%,100% {
+    transform: translate3d(1px, 3px, -2.5px);
+  }
+  50% {
+    transform: translate3d(1px, -3px, -2.5px);
+  }
+}
+@keyframes foo-after {
+  0%,100% {
+    transform: translate3d(1px, 5px, -2.5px);
+  }
+  50% {
+    transform: translate3d(1px, -5px, -2.5px);
   }
 }
 </style>
