13f52699aaaaeb0ae60dfc286897eeb68faa51ef
blackomilk <siyonghang@365rili.com>
2021-07-02 13:15:35 +0800

    样式调整

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,7 +1,61 @@
 <template>
   <div class="contact" ref="cc" id="cc">
-    <div class="title">Contact Me</div>
-    <div class="foo">Thank You!</div>
+    <!-- <div class="title">Contact Me</div>
+    <div class="foo">Thank You!</div> -->
+      <!-- 太阳 -->
+      <div class="sun star-line">
+        <div class="su-box">
+          <img src="../assets/1.png" alt="" />
+        </div>
+      </div>
+      <!-- 水星 -->
+      <div class="mercury star-line ball">
+        <!-- <div class="me-box">
+          <img src="../assets/2.png" alt="" />
+        </div> -->
+      </div>
+      <!-- 金星 -->
+      <div class="venus star-line ball ">
+        <!-- <div class="ve-box">
+          <img src="../assets/3.png" alt="" />
+        </div> -->
+      </div>
+      <!-- 地球 -->
+      <div class="earth star-line ball">
+        <!-- <div class="ea-box">
+          <img src="../assets/4.png" alt="" />
+        </div> -->
+      </div>
+      <!-- 火星 -->
+      <div class="mars star-line ball">
+        <!-- <div class="ma-box">
+          <img src="../assets/5.png" alt="" />
+        </div> -->
+      </div>
+      <!-- 木星 -->
+      <div class="jupiter star-line ball">
+        <!-- <div class="ju-box">
+          <img src="../assets/6.png" alt="" />
+        </div> -->
+      </div>
+      <!-- 土星 -->
+      <div class="saturn star-line ball">
+        <!-- <div class="sa-box">
+          <img src="../assets/7.png" alt="" />
+        </div> -->
+      </div>
+      <!-- 天王星 -->
+      <div class="uranus star-line ball">
+        <!-- <div class="ur-box">
+          <img src="../assets/8.png" alt="" />
+        </div> -->
+      </div>
+      <!-- 海王星 -->
+      <div class="neptune star-line ball">
+        <!-- <div class="ne-box">
+          <img src="../assets/9.png" alt="" />
+        </div> -->
+      </div>
   </div>
 </template>
 
@@ -10,68 +64,343 @@ export default {
   name: "contact",
   data() {
     return {
-      offsetTop: 0
-    }
+      offsetTop: 0,
+    };
   },
   watch: {
-    offsetTop(newVal,oldVal) {
-      if(newVal) {
-        console.log('newVal',newVal)
+    offsetTop(newVal, oldVal) {
+      if (newVal) {
+        console.log("newVal", newVal);
       }
-    }
+    },
   },
   mounted() {
-    let cc = document.getElementById('cc')
-    this.offsetTop = cc.offsetHeight
-    console.log('ccccccccc',cc.offsetHeight)
-
-
-    
+    let cc = document.getElementById("cc");
+    this.offsetTop = cc.offsetHeight;
+    console.log("ccccccccc", cc.offsetHeight);
   },
   methods: {
-   romanToInt(s) {
-
-    let num = 0
-    let obj = {
+    romanToInt(s) {
+      let num = 0;
+      let obj = {
         I: 1,
         V: 5,
         X: 10,
         L: 50,
         C: 100,
         D: 500,
-        M: 1000
-
-    }
-    let len = s.length
-    for(let i = 0;i<len;i++) {
-      debugger
-        let val = obj[s[i]]
-        let nVal = obj[s[i+1]]
-        if((i !== len -1 ) && val < nVal) {
-            num -= val
+        M: 1000,
+      };
+      let len = s.length;
+      for (let i = 0; i < len; i++) {
+        debugger;
+        let val = obj[s[i]];
+        let nVal = obj[s[i + 1]];
+        if (i !== len - 1 && val < nVal) {
+          num -= val;
         } else {
-            num += val
+          num += val;
         }
-    }
-    return num
-}
-  }
+      }
+      return num;
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .contact {
   width: 100%;
   height: 100vh;
-  background-color: #ffcd00;
-  padding-top: 1.1rem;
+  overflow: hidden;
+  /* background-color: #000; */
+  /* background-color: #ffcd00; */
   font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
   font-size: 1.2rem;
+  background-color: #000;
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
+  padding-top: 91vh;
+  perspective: 300px;
+    perspective-origin: 50% top;
 }
+.co {
+  width: 100%;
+  height: 50vh;
+  background-color: tan;
+}
+.star-line {
+  position: absolute;
+  perspective: 300px;
+    perspective-origin: 50% top;
+}
+.ball {
+  border-radius: 50%;
+}
+.sun,
+.su-box {
+  width: 1.5rem;
+  height: 1.5rem;
+  z-index: 11;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+.mercury {
+  position: relative;
+  width: 2rem;
+  height: 2rem;
+  border: 1px solid olive;
+  z-index: 10;
+        animation:rond 13s  linear infinite;
+
+  &::before {
+    position: absolute;
+    top: -0.07rem;
+    left: 50%;
+    content: '';
+    width: 0.15rem;
+    height: 0.15rem;
+    background: url("../assets/2.png") ;
+    background-size: 100% 100%;
+    -webkit-animation: rotate 11s linear infinite;
+  animation: rotate 11s linear infinite;
+  }
+}
+// .me-box {
+//   width: 0.2rem;
+//   height: 0.2rem;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   img {
+//     width: 100%;
+//     height: 100%;
+//   }
+// }
+.venus {
+  width: 2.7rem;
+  height: 2.7rem;
+  border: 1px solid darkgoldenrod;
+  z-index: 9;
+        animation:rond 20s  linear infinite;
+
+  &::before {
+    position: absolute;
+    top: -0.125rem;
+    left: 51%;
+    content: '';
+    width: 0.25rem;
+    height: 0.25rem;
+    background: url("../assets/3.png") ;
+    background-size: 100% 100%;
+-webkit-animation: rotate 11s linear infinite;
+  animation: rotate 11s linear infinite;
+  }
+}
+// .ve-box {
+//   width: 0.3rem;
+//   height: 0.3rem;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   img {
+//     width: 100%;
+//     height: 100%;
+//   }
+// }
+.earth {
+  width: 3.5rem;
+  height: 3.5rem;
+  border: 1px solid blue;
+  z-index: 8;
+        animation:rond 30s  linear infinite;
+
+  &::before {
+    position: absolute;
+    top: -0.17rem;
+    left: 52%;
+    content: '';
+    width: 0.39rem;
+    height: 0.35rem;
+    background: url("../assets/4.png") ;
+    background-size: 100% 100%;
+-webkit-animation: rotate 11s linear infinite;
+  animation: rotate 11s linear infinite;
+  }
+}
+// .ea-box {
+//   width: 0.35rem;
+//   height: 0.35rem;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   img {
+//     width: 100%;
+//     height: 100%;
+//   }
+// }
+.mars {
+  width: 4.1rem;
+  height: 4.1rem;
+  border: 1px solid orangered;
+  z-index: 7;
+        animation:rond 40s  linear infinite;
+
+  &::before {
+    position: absolute;
+    top: -0.1rem;
+    left: 53%;
+    content: '';
+    width: 0.29rem;
+    height: 0.2rem;
+    background: url("../assets/5.png") ;
+    background-size: 100% 100%;
+-webkit-animation: rotate 11s linear infinite;
+  animation: rotate 11s linear infinite;
+  }
+}
+// .ma-box {
+//   width: 0.4rem;
+//   height: 0.4rem;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   img {
+//     width: 100%;
+//     height: 100%;
+//   }
+// }
+.jupiter {
+  width: 6.5rem;
+  height: 6.5rem;
+  border: 1px solid yellow;
+  z-index: 6;
+        animation:rond 50s  linear infinite;
+
+  &::before {
+    position: absolute;
+    top: -0.65rem;
+    left: 55%;
+    content: '';
+    width: 1.45rem;
+    height: 1.3rem;
+    background: url("../assets/6.png") ;
+    background-size: 100% 100%;
+-webkit-animation: rotate 11s linear infinite;
+  animation: rotate 11s linear infinite;
+  }
+}
+// .ju-box {
+//   width: 0.75rem;
+//   height: 0.75rem;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   img {
+//     width: 100%;
+//     height: 100%;
+//   }
+// }
+.saturn {
+  width: 11rem;
+  height: 11rem;
+  border: 1px solid tan;
+  z-index: 5;
+        animation:rond 60s  linear infinite;
+
+  &::before {
+    position: absolute;
+    top: -0.375rem;
+    left: 45%;
+    content: '';
+    width: 0.75rem;
+    height: 0.9rem;
+    background: url("../assets/7.png") ;
+    background-size: 100% 100%;
+-webkit-animation: rotate 11s linear infinite;
+  animation: rotate 11s linear infinite;
+  }
+}
+// .sa-box {
+//   width: 0.7rem;
+//   height: 0.7rem;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   img {
+//     width: 100%;
+//     height: 100%;
+//   }
+// }
+.neptune {
+  width: 15rem;
+  height: 15rem;
+  border: 1px solid slategrey;
+  z-index: 3;
+        animation:rond 70s  linear infinite;
+
+  &::before {
+    position: absolute;
+    top: -0.325rem;
+    left: 35%;
+    content: '';
+    width: 0.65rem;
+    height: 0.8rem;
+    background: url("../assets/9.png") ;
+    background-size: 100% 100%;
+-webkit-animation: rotate 11s linear infinite;
+  animation: rotate 11s linear infinite;
+  }
+}
+// .ne-box {
+//   width: 0.35rem;
+//   height: 0.35rem;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   img {
+//     width: 100%;
+//     height: 100%;
+//   }
+// }
+.uranus {
+  width: 22rem;
+  height: 22rem;
+  border: 1px solid skyblue;
+  z-index: 4;
+      animation:rond 80s  linear infinite;
+
+}
+.uranus::before {
+    position: absolute;
+    top: -0.285rem;
+    left: 40%;
+    content: '';
+    width: 0.75rem;
+    height: 0.57rem;
+    background: url("../assets/8.png") ;
+    background-size: 100% 100%;
+  }
+// .ur-box {
+//   width: 0.35rem;
+//   height: 0.35rem;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   img {
+//     width: 100%;
+//     height: 100%;
+//   }
+// }
+
+@keyframes rond 
+  {
+     0% {transform : rotate(0deg);}
+     100% {transform : rotate(360deg);}
+  }
 .title {
   padding: 0;
   margin: 0;
